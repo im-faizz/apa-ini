22db661085aaf5e04a9d556f94390118d1efe61f
im-faizz <70867378+im-faizz@users.noreply.github.com>
2020-10-22 04:01:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,43 +13,11 @@ const start = (client = new Client()) => {
         if (state === 'CONFLICT') client.forceRefocus()
     })
 
-    // listening on message
-    client.onMessage((message) => {
-        client.getAmountOfLoadedMessages() // Cut message Cache if cache more than 3K
-            .then((msg) => {
-                if (msg >= 3000) {
-                    console.log('[CLIENT]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
-                    client.cutMsgCache()
-                }
-            })
+ 
         // Message Handler
         msgHandler(client, message)
     })
 
-    // listen group invitation
-    client.onAddedToGroup(({ groupMetadata: { id }, contact: { name } }) =>
-        client.getGroupMembersId(id)
-            .then((ids) => {
-                console.log('[CLIENT]', color(`Invited to Group. [ ${name} : ${ids.length}]`, 'yellow'))
-                // conditions if the group members are less than 10 then the bot will leave the group
-                if (ids.length <= 10) {
-                    client.sendText(id, 'Sorry, the minimum group member is 10 user to use this bot. Bye~').then(() => client.leaveGroup(id))
-                } else {
-                    client.sendText(id, `Hello group members *${name}*, thank you for inviting this bot, to see the bot menu send *#menu*`)
-                }
-            }))
-
-    client.onRemovedFromGroup((data) => {
-        // console.log(data)
-    })
-
-    // listen paricipant event on group (wellcome message)
-    client.onGlobalParicipantsChanged((event) => {
-        // if (event.action === 'add') client.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
-    })
-
-    client.onIncomingCall((callData) => {
-        // client.contactBlock(callData.peerJid)
     })
 }
 
